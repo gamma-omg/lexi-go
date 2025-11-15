a79614ce19051f3c2a6350aa08cc8e0978964aeb
Evgeny Vlasov <gamma.omg@gmail.com>
2025-11-15 17:52:50 +0200

    Tests for error WordService error paths

--- internal/words/service/word_test.go
+++ internal/words/service/word_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/gamma-omg/lexi-go.git/internal/words/model"
@@ -11,24 +12,28 @@ import (
 )
 
 type mockWordStore struct {
-	insertedWords []store.WordInsertRequest
-	deletedWords  []store.WordDeleteRequest
+	insertWord func(ctx context.Context, r store.WordInsertRequest) (string, error)
+	deleteWord func(ctx context.Context, r store.WordDeleteRequest) error
 }
 
 func (m *mockWordStore) InsertWord(ctx context.Context, r store.WordInsertRequest) (string, error) {
-	m.insertedWords = append(m.insertedWords, r)
-	return uuid.New().String(), nil
+	return m.insertWord(ctx, r)
 }
 
 func (m *mockWordStore) DeleteWord(ctx context.Context, r store.WordDeleteRequest) error {
-	m.deletedWords = append(m.deletedWords, r)
-	return nil
+	return m.deleteWord(ctx, r)
 }
 
-func Test_AddWord(t *testing.T) {
-	mockStore := &mockWordStore{}
-	service := &WordService{store: mockStore}
+func TestAddWord(t *testing.T) {
+	var insertedWords []store.WordInsertRequest
+	mockStore := &mockWordStore{
+		insertWord: func(ctx context.Context, r store.WordInsertRequest) (string, error) {
+			insertedWords = append(insertedWords, r)
+			return uuid.NewString(), nil
+		},
+	}
 
+	service := &WordService{store: mockStore}
 	req := WordAddRequest{
 		Lemma: "example",
 		Lang:  "en",
@@ -38,25 +43,75 @@ func Test_AddWord(t *testing.T) {
 	_, err := service.AddWord(context.Background(), req)
 	require.NoError(t, err)
 
-	require.Len(t, mockStore.insertedWords, 1)
-	require.Contains(t, mockStore.insertedWords, store.WordInsertRequest{
+	require.Len(t, insertedWords, 1)
+	require.Contains(t, insertedWords, store.WordInsertRequest{
 		Lemma: req.Lemma,
 		Lang:  req.Lang,
 		Class: req.Class,
 	})
 }
 
-func Test_DeleteWord(t *testing.T) {
-	mockStore := &mockWordStore{}
-	service := &WordService{store: mockStore}
+func TestAddWord_Exists(t *testing.T) {
+	mockStore := &mockWordStore{
+		insertWord: func(ctx context.Context, r store.WordInsertRequest) (string, error) {
+			return "", store.ErrExists
+		},
+	}
 
-	wordID := uuid.New().String()
+	service := &WordService{store: mockStore}
+	req := WordAddRequest{
+		Lemma: "example",
+		Lang:  "en",
+		Class: model.Noun,
+	}
+
+	_, err := service.AddWord(context.Background(), req)
+	require.Error(t, err)
+
+	se, ok := err.(*ServiceError)
+	require.True(t, ok)
+	require.Equal(t, http.StatusConflict, se.StatusCode)
+	require.Equal(t, req.Lemma, se.Env["lemma"])
+	require.Equal(t, string(req.Lang), se.Env["lang"])
+	require.Equal(t, string(req.Class), se.Env["class"])
+}
+
+func TestDeleteWord(t *testing.T) {
+	var deletedWords []store.WordDeleteRequest
+	mockStore := &mockWordStore{
+		deleteWord: func(ctx context.Context, r store.WordDeleteRequest) error {
+			deletedWords = append(deletedWords, r)
+			return nil
+		},
+	}
+
+	service := &WordService{store: mockStore}
+	wordID := uuid.NewString()
 
 	err := service.DeleteWord(context.Background(), wordID)
 	require.NoError(t, err)
 
-	require.Len(t, mockStore.deletedWords, 1)
-	require.Contains(t, mockStore.deletedWords, store.WordDeleteRequest{
+	require.Len(t, deletedWords, 1)
+	require.Contains(t, deletedWords, store.WordDeleteRequest{
 		ID: wordID,
 	})
+}
+
+func TestDeleteWord_NotFound(t *testing.T) {
+	mockStore := &mockWordStore{
+		deleteWord: func(ctx context.Context, r store.WordDeleteRequest) error {
+			return store.ErrNotFound
+		},
+	}
+
+	service := &WordService{store: mockStore}
+	wordID := uuid.NewString()
+
+	err := service.DeleteWord(context.Background(), wordID)
+	require.Error(t, err)
+
+	se, ok := err.(*ServiceError)
+	require.True(t, ok)
+	require.Equal(t, http.StatusNotFound, se.StatusCode)
+	require.Equal(t, wordID, se.Env["word_id"])
 }
